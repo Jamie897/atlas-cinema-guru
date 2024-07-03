397184159184bf43f84e55877136c63776d896dd
Jamie897 <jamiedanielle456@gmail.com>
2024-07-03 10:12:54 -0500

    .

--- my-app/src/components/general/Input.js
+++ my-app/src/components/general/Input.js
@@ -1,41 +1,41 @@
-
-
 import React from 'react';
-import './general.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './general.css';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-// src/components/general/Input.js
+const Input = ({ label, type, className, value, setValue, icon, inputAttributes, hidden, setHidden, togglePassword }) => {
 
-const Input = ({
-    label,
-    type,
-    className,
-    value,
-    setValue,
-    icon,
-    inputAttributes
-  }) => {
-    
-    const handleInput = (event) => {
-      setValue(event.target.value);
+    const handleInput = (e) => {
+        setValue(e.target.value);
     };
-  
+
+    // const inputId = inputAttributes && inputAttributes.id ? inputAttributes.id : '';
+    const inputType = type === 'password' ? 'hidden' ? 'password' : 'text' : type;
+
     return (
-      <div className={`input-wrapper ${className}`}>
-        {label && <label className="input-label">{label}</label>}
-        <div className="input-container">
-          {icon && <FontAwesomeIcon icon={icon} className="input-icon" />}
-          <input
-            type={type}
-            className="input-field"
-            value={value}
-            onChange={handleInput}
-            {...inputAttributes}
-          />
+        <div className={`input-group ${className}`}>
+            <div className='label-icon-wrapper'>
+                {icon && <FontAwesomeIcon icon={icon} className="icon-default" />}
+                {label && <label>{label}</label>}
+            </div>
+            <div className="input-wrapper">
+                <input
+                    type={inputType}
+                    value={value}
+                    onChange={handleInput}
+                    {...inputAttributes}
+                />
+            {type === 'password' && (
+                <FontAwesomeIcon
+                    icon={hidden ? faEye : faEyeSlash}
+                    className="toggle-password-icon"
+                    onClick={togglePassword}
+                />
+            )}
+            </div>
         </div>
-      </div>
     );
-  };
-  
-  export default Input;
+}
+
+export default Input;
   

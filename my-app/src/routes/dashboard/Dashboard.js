f728ee3c97abf29272ffbf82dfa29da1d8aa8470
Jamie897 <jamiedanielle456@gmail.com>
2024-07-11 08:32:19 -0500

    .

--- my-app/src/routes/dashboard/Dashboard.js
+++ my-app/src/routes/dashboard/Dashboard.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SideBar from '../../components/navigation/SideBar';
+import HomePage from './HomePage'; 
+import Favorites from './Favorites'; 
+import WatchLater from './WatchLater';
+import './Dashboard.css';
 
 const Dashboard = () => {
   return (
-    <div className="dashboard-container">
-      <SideBar />
-      <div className="dashboard-content">
-        {/* Other dashboard content */}
+    <BrowserRouter>
+      <div className="dashboard-container">
+        <SideBar />
+        <div className="dashboard-content">
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/watchlater" element={<WatchLater />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
 export default Dashboard;
+
